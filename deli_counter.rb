3bfa21df19010e8492ba22ee6d396b49d30d1f22
Bernard  Mel <bernard.mamuyac@gmail.com>
2020-03-10 00:42:59 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -8,7 +8,7 @@ def line(array)
   if current_line.size == 0
     puts "The line is currently empty."
 
-  elsif current_line.size == 1
+  else
     puts "The line is currently #{num} #{name}:"
 
   end
