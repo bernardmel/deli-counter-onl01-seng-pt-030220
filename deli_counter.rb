78c5320eb45c61d5771e2c225dd079c7a8864396
Bernard  Mel <bernard.mamuyac@gmail.com>
2020-03-10 01:02:53 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -7,7 +7,12 @@ def line(array)
   current_line = []
   if current_line.size == 0
     puts "The line is currently empty."
-
+  else
+    number=1
+    array.each do |customer|
+      current_line << "#{number}". "#{customer}"
+      number += 1
+      puts "The line is currently: #{current_line.join(" ")}""
   end
 
 end
